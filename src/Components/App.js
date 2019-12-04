6574749a67029921c4f338cd3db76c555ac03597
yukijina <yukijina@gmail.com>
2019-12-03 19:05:54 -0800

    add className

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 const App = () => {
-    return "App"
+    return (
+        <div className="ui container" style={{ marginTop: "10px"}}>
+            <SearchBar />
+        </div>
+    )
 }
 
 export default App
