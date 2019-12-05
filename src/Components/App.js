20bb5b52ed9034df60f3361cad02458a665dea69
yukijina <yukijina@gmail.com>
2019-12-04 17:04:06 -0800

    add fetch request with axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term)
+        const ACCESS = process.env.REACT_APP_ACCESS_KEY
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: `Client-ID ${ACCESS}` 
+            }
+        })
+        .then(response => {
+            console.log(response.data.results)
+        })
     }
 
     render(){
