24543fbd69d4230c3f91d8f6a2ff377ce3c67426
yukijina <yukijina@gmail.com>
2019-12-04 17:23:19 -0800

    refactor for async api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,28 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
-        const ACCESS = process.env.REACT_APP_ACCESS_KEY
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
+    state = {
+        images: []
+    }
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
             params: {
                 query: term
-            },
-            headers: {
-                Authorization: `Client-ID ${ACCESS}` 
             }
         })
-       
-            console.log(response.data.results)
-        
+            this.setState({
+                images: response.data.results
+            }) 
     }
 
     render(){
         return (
             <div className="ui container" style={{ marginTop: "10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         )
     }
