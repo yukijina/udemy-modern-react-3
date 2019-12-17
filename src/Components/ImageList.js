dc6d5fbcf4067d996eefbad5380155f8d88336f3
yukijina <yukijina@gmail.com>
2019-12-16 17:21:30 -0800

    refactor - import ImageCard

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
     // const images = props.images.map(image => {
@@ -7,9 +8,13 @@ const ImageList = props => {
     // })
 
     // refactor - images used 3 times so we can change parameters like this:
-    const images = props.images.map(({id, description, urls}) => {
-        return <img key={id} alt={description} src={urls.regular}></img>
-    })
+    // const images = props.images.map(({id, description, urls}) => {
+    //     return <img key={id} alt={description} src={urls.regular}></img>
+    // })
+
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
+    });
     return <div className="image-list">{images}</div>
 }
 
