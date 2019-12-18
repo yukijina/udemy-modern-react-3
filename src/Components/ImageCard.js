67872de5ffc56370ffe60561742ba080c99cd01e
yukijina <yukijina@gmail.com>
2019-12-17 16:21:15 -0800

    display image. addEventListener for css

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -2,14 +2,29 @@ import React from 'react';
 
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { spans: 0 }
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        //low height in css is 10px - check .image-list css
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans })
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
-                <img 
-                alt={description} 
-                src={urls.regular}></img>
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
+                <img ref={this.imageRef} alt={description} src={urls.regular}></img>
             </div>
         )
     }
